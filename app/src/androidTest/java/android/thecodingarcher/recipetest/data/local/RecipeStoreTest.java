012795b5d6a892b244cd6422a189fcec66bdf052
TheCodingArcher <rick_thegladiator@yahoo.com>
2020-01-08 20:07:42 +0530

    UI-less on-device Test : Lookup Test

--- app/src/androidTest/java/android/thecodingarcher/recipetest/data/local/RecipeStoreTest.java
+++ app/src/androidTest/java/android/thecodingarcher/recipetest/data/local/RecipeStoreTest.java
@@ -2,6 +2,7 @@ package android.thecodingarcher.recipetest.data.local;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.thecodingarcher.recipetest.data.model.Recipe;
 
 import org.junit.Test;
 
@@ -24,5 +25,15 @@ public class RecipeStoreTest {
         assertNotNull(store);
         assertNotNull(store.recipes);
         assertEquals(4, store.recipes.size());
+    }
+
+    @Test
+    public void getChocolatePudding() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        RecipeStore store = new RecipeStore(context, "recipes");
+        Recipe recipe = store.getRecipe("chocolate_pudding");
+        assertNotNull(recipe);
+        assertEquals("chocolate_pudding", recipe.id);
+        assertEquals("Chocolate Pudding", recipe.title);
     }
 }
