9812505812706f4925e65ff1591d5765cff9e664
TheCodingArcher <rick_thegladiator@yahoo.com>
2020-01-08 21:19:09 +0530

    Showing List of Recipes in our RecyclerView

--- app/src/main/java/android/thecodingarcher/recipetest/ui/main/MainActivity.java
+++ app/src/main/java/android/thecodingarcher/recipetest/ui/main/MainActivity.java
@@ -2,7 +2,10 @@ package android.thecodingarcher.recipetest.ui.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.thecodingarcher.recipetest.R;
+import android.thecodingarcher.recipetest.data.local.RecipeStore;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recipe);
+        RecipeStore store = new RecipeStore(this, "recipes");
+        RecipeAdapter adapter = new RecipeAdapter(store);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
