fc872dd3657e1bd9798f05c94b5b14721e252c65
TheCodingArcher <rick_thegladiator@yahoo.com>
2020-01-08 19:57:07 +0530

    UI-less on-device Test : Count Test

--- app/src/androidTest/java/android/thecodingarcher/recipetest/data/local/RecipeStoreTest.java
+++ app/src/androidTest/java/android/thecodingarcher/recipetest/data/local/RecipeStoreTest.java
@@ -16,4 +16,13 @@ public class RecipeStoreTest {
         assertNotNull(store.recipes);
         assertEquals(0, store.recipes.size());
     }
+
+    @Test
+    public void countRecipes() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        RecipeStore store = new RecipeStore(context, "recipes");
+        assertNotNull(store);
+        assertNotNull(store.recipes);
+        assertEquals(4, store.recipes.size());
+    }
 }
