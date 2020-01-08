8e2576366a75b4fda17a7a45d9870aa5948522ae
TheCodingArcher <rick_thegladiator@yahoo.com>
2020-01-08 22:50:10 +0530

    RecipeActivity - Loading Recipes to our Layout

--- app/src/main/java/android/thecodingarcher/recipetest/ui/recipe/RecipeActivity.java
+++ app/src/main/java/android/thecodingarcher/recipetest/ui/recipe/RecipeActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.thecodingarcher.recipetest.R;
+import android.thecodingarcher.recipetest.data.local.RecipeStore;
+import android.thecodingarcher.recipetest.data.model.Recipe;
+import android.view.View;
+import android.widget.TextView;
 
 public class RecipeActivity extends AppCompatActivity {
 
@@ -13,5 +17,20 @@ public class RecipeActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
+        TextView titleView = findViewById(R.id.title);
+        TextView descriptionView = findViewById(R.id.description);
+
+        RecipeStore store = new RecipeStore(this, "recipes");
+        String id = getIntent().getStringExtra(KEY_ID);
+        Recipe recipe = store.getRecipe(id);
+
+        if (recipe == null) {
+            titleView.setVisibility(View.GONE);
+            descriptionView.setText(R.string.recipe_not_found);
+        }
+
+        titleView.setText(recipe.title);
+        descriptionView.setText(recipe.description);
     }
 }
