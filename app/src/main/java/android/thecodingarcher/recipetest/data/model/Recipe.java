3dd34e43dec91e42549bc6f2a428ae619aff7226
TheCodingArcher <rick_thegladiator@yahoo.com>
2020-01-08 14:06:47 +0530

    Smallest Unit Test Class

---
+++ app/src/main/java/android/thecodingarcher/recipetest/data/model/Recipe.java
@@ -0,0 +1,19 @@
+package android.thecodingarcher.recipetest.data.model;
+
+import java.io.InputStream;
+
+public class Recipe {
+    public final String id;
+    public final String title;
+    public final String description;
+
+    public Recipe(String id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public Recipe readFromStream(InputStream stream) {
+        return null;
+    }
+}
